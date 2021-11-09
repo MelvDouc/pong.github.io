1d0d19108bb486bea2dd7cc326ec238726633513
MelvDouc <melv.douc@gmail.com>
2021-11-09 20:28:16 +0100

    Change key mapping (3)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Controls
 
-- Press `W` / `Alt` to move the left paddle up / down.
+- Press `Y` / `H` to move the left paddle up / down.
 - Press `↑` / `↓` to move the right paddle up / down.
 
 [Play](https://melvdouc.github.io/pong.github.io/)
--- static/js/PongCanvas.js
+++ static/js/PongCanvas.js
@@ -25,11 +25,11 @@ export default class PongCanvas extends HTMLCanvasElement {
     }
     static movePaddles(e) {
         switch (e.key) {
-            case "w":
+            case "y":
                 if (this.leftPaddle.top > 0)
                     this.leftPaddle.moveUp();
                 break;
-            case "Alt":
+            case "h":
                 if (this.leftPaddle.bottom < this.cHeight)
                     this.leftPaddle.moveDown();
                 break;
